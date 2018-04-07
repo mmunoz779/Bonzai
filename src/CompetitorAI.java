3ffeb9614af86eb20e6e1d5cff02f92b49e8efb0
Aidan <36714861+adgvandrie@users.noreply.github.com>
2018-04-07 14:40:24 -0400

    added collect method

--- src/CompetitorAI.java
+++ src/CompetitorAI.java
@@ -72,33 +72,7 @@ public class CompetitorAI implements ElementsAI {
                  *
                  * To access data of rival teams, try gameState.rivals() or gameState.leadingRival().
                  */
-                Collection<Earth> earthsWithCrystals = gameState.my().get(Elements.EARTH, ElementsFilters.HAS_CRYSTAL);
-                earthsWithCrystals.forEach(earth -> {
-                    /*
-                     * Some commands have default behaviors if they cannot be carried out immediately.
-                     * For instance, deposit crystal will move the earth closer to the crystal if it can't
-                     * pick it up this turn.
-                     */
-                    earth.depositCrystal();
-                });
-
-                Collection<Earth> earthsWithoutCrystals = gameState.my().get(Elements.EARTH, ElementsFilters.HAS_CRYSTAL.negate());
-                earthsWithoutCrystals.forEach(earth -> {
-                    /*
-                     * There are several different path finding methods that will come in handy as
-                     * you navigate the elementals through the world.
-                     */
-                    Crystal closestCrystal = earth.pathfinding().findNearest(Crystal.class);
-                    if (closestCrystal != null) {
-                        earth.pickUpCrystal(closestCrystal);
-                    }
-                });
-
-                /*
-                 * You don't need to know functional programming to compete in BonzAI, but we highly
-                 * recommend learning it if you get a chance.
-                 */
-                gameState.my().airs().forEach(Air::disperse);
+                collect(crystals);
             }
         }
     }
